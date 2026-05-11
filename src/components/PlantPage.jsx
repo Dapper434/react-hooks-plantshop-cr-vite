6d7e4f1e4bcf3548079d55e14f7ada0a989cf70b
Pipsy <gpflaslik7112@gmail.com>
2026-05-11 22:03:53 +0300

    feat: add componenet functionality

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -4,18 +4,25 @@ import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
-const [plants, setPlants] = useState([]);
-const [searchQuery, setSearchQuery] = useState("");
-useEffect(() => {
-  fetch("http://localhost:6001/plants")
-    .then((response) => response.json())
-    .then((data) => setPlants(data));
-}, []);
+  const [plants, setPlants] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((data) => setPlants(data));
+  }, []);
+  
+  
+  const plantsToDisplay = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   
   return (
     <main>
       <NewPlantForm />
       <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+     
       <PlantList plants={plantsToDisplay} />
     </main>
   );
